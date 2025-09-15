e106b47ff14383d5ed65dad686e9c200ee94def0
FayzDEv <fayzullajon07@gmail.com>
2025-09-15 12:36:52 +0900

    1-dars.py
    bu birinchi dars bu darsda siz print funksiyasi bilan tanishasiz agar shu darsni mukammal o'rganmoqchi bo'lsangiz www.youtube.com/@FayzDev youtube kanalidan o'rganishingiz mumkin

---
+++ 1-dars.py
@@ -0,0 +1,10 @@
+#sana: 13.09.2025
+#muallif: FayzDev
+#telegram: @FayzDev
+#youtube: @FayzDev
+#1-dars
+#mavzu print() funksiyasi bilan tanishuv 
+
+print ("hello worlid")
+print (15*5)
+print(15*5)
